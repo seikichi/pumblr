bd6d70805a856515ae9979ba3a22e9502e56e5bc
seikichi <seikichi@kmc.gr.jp>
2010-09-12 16:05:10 +0900

    api/readのオプションを追加した(全てでは無い)

--- pumblr/__init__.py
+++ pumblr/__init__.py
@@ -1,1 +1,5 @@
-#
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from api import API
+api = API()
--- pumblr/api.py
+++ pumblr/api.py
@@ -13,10 +13,33 @@ class API(object):
     """Tumblr API"""
 
     def __init__(self):
-        """
-        """
         pass
 
-    def read(self, url):
-        url = "http://%s/api/read/json" % url
-        return ApiRead.parse(json.loads(utils.extract_dict(urllib2.urlopen(url).read())))
+    def read(self, user, start=0, num=20, type=None, id=None, search=None, tagged=None):
+        """
+        Arguments:
+        - `user`: username
+        - `start`: The post offset to start from. The default is 0.
+        - `num`: The number of posts to return. The default is 20, and the maximum is 50.
+        - `type`: The type of posts to return. If unspecified or empty, all types of posts are returned. Must be one of text, quote, photo, link, chat, video, or audio.
+        - `id`: A specific post ID to return. Use instead of start, num, or type.
+        - `search`: Search for posts with this query.
+        - `tagged`: Return posts with this tag in reverse-chronological order (newest first).
+        """
+        if id is not None:
+            query = dict(id=id)
+        else:
+            query = dict(
+                start=start,
+                num=num
+            )
+            if type is not None:
+                query['type'] = type
+            if search is not None:
+                query['search'] = search
+            if tagged is not None:
+                query['tagged'] = tagged
+
+        url = "http://%s.tumblr.com/api/read/json" % user
+        req = urllib2.urlopen(url+'?'+urlencode(query))
+        return ApiRead.parse(json.loads(utils.extract_dict(req.read())))
