ba5d87ff551df47df2ed4de15058df28ad49fe41
seikichi <seikichi@kmc.gr.jp>
2010-09-12 13:49:23 +0900

    add error classes.

---
+++ pumblr/error.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+class PumblrError(object):
+    """Pumblr exception"""
+    
+    def __init__(self, msg):
+        self._msg = msg
+
+    def __str__(self):
+        return self._msg
+
+
+class PumblrAuthError(PumblrError):
+    """403 Forbidden exception"""
+    pass
+
+
+class PumblrReqestError(PumblrError):
+    """400 Bad Request exception"""
+    pass
+
