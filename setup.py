88c8b297f9d7180c83119e771127d467c1af6014
seikichi <seikichi@kmc.gr.jp>
2010-09-13 11:35:25 +0900

    modify install_requires in setup.py (add 'simplejson')

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(name='pumblr',
       include_package_data=True,
       zip_safe=False,
       install_requires=[
-          # -*- Extra requirements: -*-
+          'simplejson'
       ],
       entry_points="""
       # -*- Entry points: -*-
