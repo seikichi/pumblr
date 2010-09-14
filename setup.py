067984d6c163b28589f8d3047946c0d82227be22
seikichi <seikichi@kmc.gr.jp>
2010-09-14 17:33:03 +0900

    modify setup.py (remove 'simplejson' in install_requires)
    py2.6以降だとjson入ってるから足りるのにinstallしちゃうので．．．

--- setup.py
+++ setup.py
@@ -23,7 +23,6 @@ setup(name='pumblr',
       include_package_data=True,
       zip_safe=False,
       install_requires=[
-          'simplejson'
       ],
       entry_points="""
       # -*- Entry points: -*-
